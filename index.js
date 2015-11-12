850ce04ce9fc3571c56b4cbbbf4af3aeac2d3365
Gerard <g.sole.ca@gmail.com>
2015-11-12 15:43:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ module.exports.init = function(injector, oauthConfig){
 	oauthConfig.authorize_uri = '/oauth/authorize';
 	oauthConfig.login_uri = '/oauth/login';
 	oauthConfig.access_token_uri = '/oauth/token';
+	module.exports.preInject = function(app, cb) {
 	injector.security = require('./lib')(injector, oauthConfig);
+		return cb();
+	}
 }
